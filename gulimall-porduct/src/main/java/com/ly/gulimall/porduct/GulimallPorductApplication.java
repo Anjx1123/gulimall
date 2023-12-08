46267bd8865503e23e45cae319634ee38b10fcbf
liuyang <1072603842@qq.com>
2023-12-08 10:35:36 +0800

    项目初始化

---
+++ gulimall-porduct/src/main/java/com/ly/gulimall/porduct/GulimallPorductApplication.java
@@ -0,0 +1,13 @@
+package com.ly.gulimall.porduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallPorductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallPorductApplication.class, args);
+    }
+
+}
---
+++ gulimall-porduct/src/test/java/com/ly/gulimall/porduct/GulimallPorductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.gulimall.porduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallPorductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
